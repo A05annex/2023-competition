e1dee6035b0421602bb7ec371b42a68c6a31ddf3
Ethan Ready <ethanready@icloud.com>
2023-02-01 09:23:15 -0800

    cleaned up and commented AutoBalanceCommand. added rotation control

--- src/main/java/frc/robot/commands/AutoBalanceCommand.java
+++ src/main/java/frc/robot/commands/AutoBalanceCommand.java
@@ -5,16 +5,22 @@ import edu.wpi.first.wpilibj2.command.CommandBase;
 import org.a05annex.frc.NavX;
 import org.a05annex.frc.subsystems.DriveSubsystem;
 import org.a05annex.util.AngleConstantD;
+import org.a05annex.util.AngleD;
 
-
+/**
+ * AutoBalanceCommand moves robot forward or backward based on robot tilt
+ */
 public class AutoBalanceCommand extends CommandBase {
     private final DriveSubsystem m_driveSubsystem = DriveSubsystem.getInstance();
-
+    private final NavX m_navX = NavX.getInstance();
     // you need to use roll as pitch and roll return the opposite values of what you expect
-    private AngleConstantD m_pitch = NavX.getInstance().getNavInfo().roll;
-    // track during how many cylces (20ms) the robot was balanced
-    private int ticksBalanced = 0;
+    private AngleConstantD m_pitch = m_navX.getNavInfo().roll;
 
+    // how fast should the robot drive (0.0 - 1.0) when trying to balance
+    private final double speed = 0.15;
+
+    // track during how many cycles (20ms) the robot was balanced
+    private int ticksBalanced = 0;
     private boolean isFinished = false;
 
 
@@ -29,18 +35,30 @@ public class AutoBalanceCommand extends CommandBase {
         isFinished = false;
         ticksBalanced = 0;
         m_pitch = NavX.getInstance().getNavInfo().roll;
+
+        // set expected heading to either 0 or 180, whichever is closer to current heading
+        if (m_navX.getHeading().isGreaterThan(AngleConstantD.DEG_NEG_90) && m_navX.getHeading().isLessThanOrEqual(AngleConstantD.DEG_90)) {
+            m_navX.setExpectedHeading(new AngleD().setDegrees(0.0));
+        }
+        else {
+            m_navX.setExpectedHeading(new AngleD().setDegrees(180.0));
+        }
     }
 
     @Override
     public void execute() {
         m_pitch = NavX.getInstance().getNavInfo().roll; // set pitch again because getNavInfo does not auto update
         if (m_pitch.getDegrees() > 10) {
-            m_driveSubsystem.swerveDrive(new AngleConstantD(AngleConstantD.ZERO), 0.15, 0.0);
+            // drive backward and reset ticks balanced when tipped forward
+            m_driveSubsystem.swerveDrive(new AngleConstantD(AngleConstantD.ZERO), -speed, 0.0);
             ticksBalanced = 0;
         } else if (m_pitch.getDegrees() < -10) {
-            m_driveSubsystem.swerveDrive(new AngleConstantD(AngleConstantD.ZERO), -0.15, 0.0);
+            // drive forward and reset ticks balanced when tipped backward
+            m_driveSubsystem.swerveDrive(AngleConstantD.ZERO, speed, 0.0);
             ticksBalanced = 0;
         } else {
+            // Stop robot and increment ticks balanced while not at full tilt
+            m_driveSubsystem.swerveDrive(AngleConstantD.ZERO, 0.0, 0.0);
             ticksBalanced++;
         }
 
@@ -51,6 +69,7 @@ public class AutoBalanceCommand extends CommandBase {
 
     @Override
     public boolean isFinished() {
+        // Is ticks balanced is greater than 150? ends the command if it is. 1 tick = 20ms. 20ms * 150 = 3 seconds
         if(ticksBalanced > 150) {
             isFinished = true;
         }
