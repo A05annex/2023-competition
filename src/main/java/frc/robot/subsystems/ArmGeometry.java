76b730149a8e30e3257a7806e36596b9fe67567d
STHobbes <royster.hall@gmail.com>
2023-02-15 16:21:40 -0700

    The math and tests for the arm geometry.

--- src/main/java/frc/robot/subsystems/ArmGeometry.java
+++ src/main/java/frc/robot/subsystems/ArmGeometry.java
@@ -2,58 +2,136 @@ package frc.robot.subsystems;
 
 import org.a05annex.util.AngleD;
 import org.a05annex.util.Utl;
+import org.a05annex.util.geo3d.Point3d;
+import org.jetbrains.annotations.NotNull;
 
 import java.awt.geom.Point2D;
 
 /**
- * This is a helper class that contains all of the math for the arm subsystem. The math is
+ * This is a helper class that contains all the geometry math for the arm subsystem. The math is
  * abstracted into this class so we can easily do automated testing, and so arm geometry information
  * is available to other subsystems and commands that may need access to that information.
  */
 public class ArmGeometry {
 
+    /**
+     * A class representing arm positions
+     */
     public static class ArmPositions {
-        public final double pivotPosition;
-        public final double extensionPosition;
+        private double pivotPosition;
+        private double extensionPosition;
+
         ArmPositions(double pivotPosition, double extensionPosition) {
             this.pivotPosition = pivotPosition;
             this.extensionPosition = extensionPosition;
+        }
+
+        public double getPivotPosition() {
+            return pivotPosition;
+        }
+
+        public double getExtensionPosition() {
+            return extensionPosition;
+        }
+
+        /**
+         * @return {@code true} if the current values were valid for the robot during setup,
+         * {@code false} if the values were clipped in some way to make the position valid at setup.
+         */
+        public boolean clipToValidAtSetup() {
+            return true;
+        }
+
+        /**
+         * Clip the position so all positions are valid and so that the end of the arm is inside the
+         * allowable extension zone (48" horizontally beyond the frame), and above the ground. The strategy is to
+         * first clip the positions to the allowable bounds, then that is converted to location, and if the
+         * location is outside the extension zone or below the ground, the arm is shortened to make the position
+         * without changing the pivot angle.
+         *
+         * @return {@code true} if the current values were valid for competition play,
+         * {@code false} if the values were clipped in some way to make the position valid during play.
+         */
+        public boolean clipToValidInPlay() {
+            double originalPivotPosition = pivotPosition;
+            double originalExtensionPosition = extensionPosition;
+            // first clip - clip the positions to the valid position ranges
+            pivotPosition = Utl.clip(pivotPosition, PIVOT_MIN_POSITION, PIVOT_MAX_POSITION);
+            extensionPosition = (EXTENSION_POSITION_MIN_ALLOWABLE < EXTENSION_POSITION_MAX_ALLOWABLE) ?
+                    Utl.clip(extensionPosition, EXTENSION_POSITION_MIN_ALLOWABLE,EXTENSION_POSITION_MAX_ALLOWABLE) :
+                    Utl.clip(extensionPosition, EXTENSION_POSITION_MAX_ALLOWABLE,EXTENSION_POSITION_MIN_ALLOWABLE);
+            // get back to a position
+            Point2D.Double location = getArmLocationFromPositions(pivotPosition,extensionPosition);
+            AngleD pivotAngle = getPivotAngleFromPosition(pivotPosition);
+            boolean clippedLocation = false;
+            // check position against the extension zone boundary
+            double maxX = HORIZONTAL_ARM_PIVOT_TO_FRAME + HORIZONTAL_EXTENSION_PAST_FRAME_LIMIT -
+                    HORIZONTAL_EXTENSION_SAFETY_BUFFER;
+            if (location.x > maxX) {
+                location.x = maxX;
+                location.y = maxX / pivotAngle.tan();
+                clippedLocation = true;
+            }
+            // check the position against the ground boundary
+            double minY = (-VERTICAL_FLOOR_TO_ARM_PIVOT) + FLOOR_EXTENSION_SAFETY_BUFFER;
+            if (location.y < minY) {
+                location.x = minY * pivotAngle.tan();;
+                location.y = minY;
+                clippedLocation = true;
+            }
+            // OK, now if the location was clipped, get the new positions
+            if (clippedLocation) {
+                ArmPositions newPositions = getArmPositionsFromLocation(location.x, location.y);
+                pivotPosition = newPositions.pivotPosition;
+                extensionPosition = newPositions.extensionPosition;
+            }
+
+            return (originalPivotPosition == pivotPosition) && (originalExtensionPosition == extensionPosition);
         }
     }
 
     public static final double PIVOT_TICS_PER_ROTATION = 30.309 * 4.0; //Reading from 0 to 90 degrees * 4 = full rotation
 
     public static final double PIVOT_MIN_POSITION = 0.0;
-    public static final double PIVOT_MAX_POSITION = 45.0;
+    public static final double PIVOT_MAX_POSITION = 44.0;
 
     /**
      * The extension position encoder when the arm is against the minimum extension limit.
      */
-    public static final double EXTENSION_POSITION_MINIMUM_EXTENSION = 210;
+    public static final double EXTENSION_POSITION_MIN_EXTENSION = 210;
     /**
      * The arm length (in inches) physically measured when the arm is against the minimum extension limit.
      */
-    public static final double ARM_LENGTH_MINIMUM_EXTENSION = 22.5;
+    public static final double ARM_LENGTH_MIN_EXTENSION = 22.5;
     /**
      * The extension position encoder when the arm is against the maximum extension limit.
      */
-    public static final double EXTENSION_POSITION_MAXIMUM_EXTENSION = 0;
+    public static final double EXTENSION_POSITION_MAX_EXTENSION = 0;
     /**
      * The arm length (in inches) physically measured when the arm is against the maximum extension limit.
      */
-    public static final double ARM_LENGTH_MAXIMUM_EXTENSION = 77.78;
+    public static final double ARM_LENGTH_MAX_EXTENSION = 77.78;
     /**
      * This is a cushion in the requested extension position that allows about 3/8" between the menimum-maximum
      * end position and the physical stop. This buffer insures that the requested encoder position is inside
      * the stop so the extension motor is never stalled trying to exceed the stop.
      */
     public static final double EXTENSION_POSITION_CUSHION = 1.5;
+
+    @SuppressWarnings("ConstantValue")
+    public static final double EXTENSION_POSITION_MIN_ALLOWABLE = EXTENSION_POSITION_MIN_EXTENSION +
+            (EXTENSION_POSITION_MIN_EXTENSION > EXTENSION_POSITION_MAX_EXTENSION ?
+            -EXTENSION_POSITION_CUSHION : EXTENSION_POSITION_CUSHION);
+    @SuppressWarnings("ConstantValue")
+    public static final double EXTENSION_POSITION_MAX_ALLOWABLE = EXTENSION_POSITION_MAX_EXTENSION +
+            (EXTENSION_POSITION_MIN_EXTENSION > EXTENSION_POSITION_MAX_EXTENSION ?
+            EXTENSION_POSITION_CUSHION : -EXTENSION_POSITION_CUSHION);
     /**
      * The extension encoder tics per inch, calculated from the tics between
      */
     public static final double EXTENSION_TICS_PER_INCH =
-            (EXTENSION_POSITION_MAXIMUM_EXTENSION - EXTENSION_POSITION_MINIMUM_EXTENSION) /
-                    (ARM_LENGTH_MAXIMUM_EXTENSION - ARM_LENGTH_MINIMUM_EXTENSION);
+            (EXTENSION_POSITION_MAX_EXTENSION - EXTENSION_POSITION_MIN_EXTENSION) /
+                    (ARM_LENGTH_MAX_EXTENSION - ARM_LENGTH_MIN_EXTENSION);
 
     /**
      * The horizontal distance (in inches) from the arm pivot to the edge of the frame.
@@ -62,7 +140,7 @@ public class ArmGeometry {
     /**
      * The vertical distance fr4om the floor to the arm pivot.
      */
-    public static final double VERTICAL_FLOOR_TO_ARM_PIVOT = 18.0; // Need a real distance for this, my guess.
+    public static final double VERTICAL_FLOOR_TO_ARM_PIVOT = 18.25; // Need a measurement
 
     /**
      * The horizontal distance (in inches) the game rules allow a robot appendage to extend from the frame.
@@ -74,45 +152,87 @@ public class ArmGeometry {
      * the arm control from explicitly following the specified settings.
      */
     public static final double HORIZONTAL_EXTENSION_SAFETY_BUFFER = 5.0;
+    public static final double FLOOR_EXTENSION_SAFETY_BUFFER = 3.0;
 
     /**
      * The method computes the current arm length from the reported extension encoder position.
+     *
      * @return The current arm length
      */
     public static double getArmLengthFromExtensionPosition(double extensionPosition) {
-        return ARM_LENGTH_MINIMUM_EXTENSION + (((extensionPosition - EXTENSION_POSITION_MINIMUM_EXTENSION)/
-                (EXTENSION_POSITION_MAXIMUM_EXTENSION - EXTENSION_POSITION_MINIMUM_EXTENSION)) *
-                (ARM_LENGTH_MAXIMUM_EXTENSION - ARM_LENGTH_MINIMUM_EXTENSION));
+        return ARM_LENGTH_MIN_EXTENSION + (((extensionPosition - EXTENSION_POSITION_MIN_EXTENSION) /
+                (EXTENSION_POSITION_MAX_EXTENSION - EXTENSION_POSITION_MIN_EXTENSION)) *
+                (ARM_LENGTH_MAX_EXTENSION - ARM_LENGTH_MIN_EXTENSION));
     }
 
     /**
      * This method computes the extension encoder position that would produce the requested arm length if the
      * geometry of the arm were unconstrained. It is the caller's responsibility to determine what should be
      * done if the encoder position is outside the physical limits of the arm.
+     *
      * @param armLength The desired arm length.
      * @return The encoder position to achieve the requested length.
      */
     public static double getExtensionPositionFromArmLength(double armLength) {
 
-        return  EXTENSION_POSITION_MINIMUM_EXTENSION +
-                (((armLength - ARM_LENGTH_MINIMUM_EXTENSION) /
-                        (ARM_LENGTH_MAXIMUM_EXTENSION - ARM_LENGTH_MINIMUM_EXTENSION)) *
-                        (EXTENSION_POSITION_MAXIMUM_EXTENSION - EXTENSION_POSITION_MINIMUM_EXTENSION));
+        return EXTENSION_POSITION_MIN_EXTENSION +
+                (((armLength - ARM_LENGTH_MIN_EXTENSION) /
+                        (ARM_LENGTH_MAX_EXTENSION - ARM_LENGTH_MIN_EXTENSION)) *
+                        (EXTENSION_POSITION_MAX_EXTENSION - EXTENSION_POSITION_MIN_EXTENSION));
     }
 
+    /**
+     * Get the pivot angle from the pivot position. This transformation does no
+     * validity checking or clipping.
+     *
+     * @param pivotPosition The pivot position.
+     * @return The pivot angle corresponding to the pivot position.
+     */
     public static AngleD getPivotAngleFromPosition(double pivotPosition) {
-        return new AngleD().setDegrees((pivotPosition * 360.0)/ PIVOT_TICS_PER_ROTATION);
-
+        return new AngleD().setDegrees((pivotPosition * 360.0) / PIVOT_TICS_PER_ROTATION);
     }
+
+    /**
+     * Get the pivot position from the pivot angle. This transformation does no
+     * validity checking or clipping.
+     *
+     * @param pivotAngle The pivot angle.
+     * @return The pivot position corresponding to the pivot angle.
+     */
     public static double getPivotPositionFromAngle(AngleD pivotAngle) {
         return (pivotAngle.getDegrees() * PIVOT_TICS_PER_ROTATION) / 360.0;
 
     }
+
+    /**
+     * Convert pivot and extension positions to a location in 2D space where the pivot
+     * is at (0.0,0.0), X is horizontal towards the frame, and Y is vertical. This
+     * method does not consider validity of the location or the encoder positions when making
+     * this conversion.
+     *
+     * @param pivotPosition The pivot position.
+     * @param extensionPosition The extension position.
+     * @return The location of the end of the arm.
+     */
+    @NotNull
     public static Point2D.Double getArmLocationFromPositions(double pivotPosition, double extensionPosition) {
-        return new Point2D.Double(0.0, 0.0);
+        AngleD pivotAngle = getPivotAngleFromPosition(pivotPosition);
+        double armLength = getArmLengthFromExtensionPosition(extensionPosition);
+        return new Point2D.Double(armLength * pivotAngle.sin(), armLength * pivotAngle.cos());
     }
 
+    /**
+     * Convert a position relative to the pivot at (0.0,0.0) to encoder positions to achieve that position. This
+     * method does not consider validity of the location or the encoder positions when making this conversion.
+     *
+     * @param x The X position relative to the pivot at (0.0,0.0).
+     * @param y The Y position relative to the pivot at (0.0,0.0).
+     * @return Returns the encoder positions required to position the arm at the specified location.
+     */
+    @NotNull
     public static ArmPositions getArmPositionsFromLocation(double x, double y) {
-        return new ArmPositions(0.0, EXTENSION_POSITION_MAXIMUM_EXTENSION);
+        double armLength = Utl.length(x, y);
+        AngleD pivotAngle = new AngleD().atan2(x, y);
+        return new ArmPositions(getPivotPositionFromAngle(pivotAngle), getExtensionPositionFromArmLength(armLength));
     }
 }
--- src/test/java/frc/robot/subsystem/TestArmGeometry.java
+++ src/test/java/frc/robot/subsystem/TestArmGeometry.java
@@ -2,8 +2,11 @@ package frc.robot.subsystem;
 
 import frc.robot.subsystems.ArmGeometry;
 import org.a05annex.util.AngleD;
+import org.a05annex.util.Utl;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.awt.geom.Point2D;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -12,30 +15,31 @@ public class TestArmGeometry {
     @Test
     @DisplayName("test EXTENSION_TICS_PER_INCH")
     void TestExtensionTicsPerInch() {
-        System.out.println("---------------------------------------------------------------------------------;");
+        System.out.println("---------------------------------------------------------------------------------");
         System.out.println(String.format("Extension tics per inch = %.4f", ArmGeometry.EXTENSION_TICS_PER_INCH));
     }
 
     @Test
     @DisplayName("test conversions Arm Length to/from Encoder Position")
     void TestLengthEncoderPositionConversion() {
-        System.out.println("---------------------------------------------------------------------------------;");
+        System.out.println("---------------------------------------------------------------------------------");
         // verify minimum and maximum extension
-        assertEquals(ArmGeometry.ARM_LENGTH_MINIMUM_EXTENSION,
-                ArmGeometry.getArmLengthFromExtensionPosition(ArmGeometry.EXTENSION_POSITION_MINIMUM_EXTENSION), 0.0001);
-        assertEquals(ArmGeometry.ARM_LENGTH_MAXIMUM_EXTENSION,
-                ArmGeometry.getArmLengthFromExtensionPosition(ArmGeometry.EXTENSION_POSITION_MAXIMUM_EXTENSION), 0.0001);
-        assertEquals(ArmGeometry.EXTENSION_POSITION_MINIMUM_EXTENSION,
-                ArmGeometry.getExtensionPositionFromArmLength(ArmGeometry.ARM_LENGTH_MINIMUM_EXTENSION), 0.0001);
-        assertEquals(ArmGeometry.EXTENSION_POSITION_MAXIMUM_EXTENSION,
-                ArmGeometry.getExtensionPositionFromArmLength(ArmGeometry.ARM_LENGTH_MAXIMUM_EXTENSION), 0.0001);
-        // run a sequence and verify a round-trip from position to length to position gets back to the same position.
-        for (double encoderPosition = ArmGeometry.EXTENSION_POSITION_MINIMUM_EXTENSION;
-             ArmGeometry.EXTENSION_POSITION_MAXIMUM_EXTENSION < ArmGeometry.EXTENSION_POSITION_MINIMUM_EXTENSION ?
-                     encoderPosition >= ArmGeometry.EXTENSION_POSITION_MAXIMUM_EXTENSION :
-                     encoderPosition <= ArmGeometry.EXTENSION_POSITION_MAXIMUM_EXTENSION;
+        assertEquals(ArmGeometry.ARM_LENGTH_MIN_EXTENSION,
+                ArmGeometry.getArmLengthFromExtensionPosition(ArmGeometry.EXTENSION_POSITION_MIN_EXTENSION), 0.0001);
+        assertEquals(ArmGeometry.ARM_LENGTH_MAX_EXTENSION,
+                ArmGeometry.getArmLengthFromExtensionPosition(ArmGeometry.EXTENSION_POSITION_MAX_EXTENSION), 0.0001);
+        assertEquals(ArmGeometry.EXTENSION_POSITION_MIN_EXTENSION,
+                ArmGeometry.getExtensionPositionFromArmLength(ArmGeometry.ARM_LENGTH_MIN_EXTENSION), 0.0001);
+        assertEquals(ArmGeometry.EXTENSION_POSITION_MAX_EXTENSION,
+                ArmGeometry.getExtensionPositionFromArmLength(ArmGeometry.ARM_LENGTH_MAX_EXTENSION), 0.0001);
+        // run a sequence of encode positions from minimum tp maximum and verify a round-trip from position
+        // to length to position gets back to the same position.
+        for (double encoderPosition = ArmGeometry.EXTENSION_POSITION_MIN_EXTENSION;
+             ArmGeometry.EXTENSION_POSITION_MAX_EXTENSION < ArmGeometry.EXTENSION_POSITION_MIN_EXTENSION ?
+                     encoderPosition >= ArmGeometry.EXTENSION_POSITION_MAX_EXTENSION :
+                     encoderPosition <= ArmGeometry.EXTENSION_POSITION_MAX_EXTENSION;
              encoderPosition +=
-                     ArmGeometry.EXTENSION_POSITION_MAXIMUM_EXTENSION < ArmGeometry.EXTENSION_POSITION_MINIMUM_EXTENSION ?
+                     ArmGeometry.EXTENSION_POSITION_MAX_EXTENSION < ArmGeometry.EXTENSION_POSITION_MIN_EXTENSION ?
                              -10.0 : 10.0) {
             double positionToLength = ArmGeometry.getArmLengthFromExtensionPosition(encoderPosition);
             double lengthToPosition = ArmGeometry.getExtensionPositionFromArmLength(positionToLength);
@@ -48,7 +52,9 @@ public class TestArmGeometry {
     @Test
     @DisplayName("test conversions pivot angle to/from Encoder Position")
     void TestPivotEncoderAngleConversion() {
-        System.out.println("---------------------------------------------------------------------------------;");
+        System.out.println("---------------------------------------------------------------------------------");
+        // Run a sequence of encoder positions from minimum to maximum and verify round trip conversions get
+        // back to the same encoder values.
         for (double pivotPosition = ArmGeometry.PIVOT_MIN_POSITION;
              pivotPosition <= ArmGeometry.PIVOT_MAX_POSITION; pivotPosition += 5.0) {
             AngleD positionToAngle = ArmGeometry.getPivotAngleFromPosition(pivotPosition);
@@ -58,5 +64,89 @@ public class TestArmGeometry {
             assertEquals(pivotPosition, pivotPosition, 0.00001);
         }
     }
+
+    private void locationToFromPosition(double x, double y) {
+        ArmGeometry.ArmPositions positions = ArmGeometry.getArmPositionsFromLocation(x, y);
+        Point2D.Double location = ArmGeometry.getArmLocationFromPositions(
+                positions.getPivotPosition(), positions.getExtensionPosition());
+        System.out.println(String.format(
+                "location: (%7.2f,%7.2f); positions: (%7.2f,%7.2f); back to location: (%7.2f,%7.2f)",
+                x, y, positions.getPivotPosition(), positions.getExtensionPosition(),
+                location.x, location.y ));
+        assertEquals(x, location.x, 0.00001);
+        assertEquals(y, location.y, 0.00001);
+    }
+    @Test
+    @DisplayName("test location to/from positions")
+    void TestLocationToFromPositions() {
+        double x, y;
+        System.out.println("---------------------------------------------------------------------------------");
+        System.out.println("TestLocationToFromPositions - no clipping for valid extent ");
+        System.out.println("---------------------------------------------------------------------------------");
+        // run a sequence from (0",0") to (90",0") by 5" increments
+        for (x = 0, y = 0; x <= 90.0; x += 5.0) {
+            locationToFromPosition(x,y);
+        }
+        System.out.println("---------------------------------------------------------------------------------");
+        // run a sequence from (0",30") to (90",0") by 5" increments
+        for (x = 0, y = 30; x <= 90.0; x += 5.0) {
+            locationToFromPosition(x,y);
+        }
+        System.out.println("---------------------------------------------------------------------------------");
+        // run a sequence from (0",60") to (90",0") by 5" increments
+        for (x = 0, y = 60; x <= 90.0; x += 5.0) {
+            locationToFromPosition(x,y);
+        }
+        System.out.println("---------------------------------------------------------------------------------");
+        // run a sequence from (50",-30") to (50",120") by 5" increments
+        for (x = 50, y = -30; y <= 120.0; y += 5.0) {
+            locationToFromPosition(x,y);
+       }
+        System.out.println("---------------------------------------------------------------------------------");
+        // run a sequence from (20",-30") to (20",120") by 5" increments
+        for (x = 20, y = -30; y <= 120.0; y += 5.0) {
+            locationToFromPosition(x,y);
+        }
+    }
+
+    @Test
+    @DisplayName("test clipping, but dong an angle sweep with invalid long length")
+    void TestClippingBySweep() {
+        System.out.println("---------------------------------------------------------------------------------");
+        for (double degrees = -5.0; degrees <= 150.0; degrees += 5.0) {
+            AngleD pivotAngle = new AngleD().setDegrees(degrees);
+            double x = 120.0 * pivotAngle.sin();
+            double y = 120.0 * pivotAngle.cos();
+            ArmGeometry.ArmPositions positions = ArmGeometry.getArmPositionsFromLocation(x, y);
+            boolean clipped = positions.clipToValidInPlay();
+            Point2D.Double location = ArmGeometry.getArmLocationFromPositions(
+                    positions.getPivotPosition(), positions.getExtensionPosition());
+            System.out.println(String.format(
+                    "location: (%7.2f,%7.2f); positions: (%7.2f,%7.2f) - valid:%b; back to location: (%7.2f,%7.2f)",
+                    x, y, positions.getPivotPosition(), positions.getExtensionPosition(), clipped,
+                    location.x, location.y ));
+        }
+    }
+    @Test
+    @DisplayName("test old clipping, but dong an angle sweep with invalid long length")
+    void TestOldClippingBySweep() {
+        System.out.println("---------------------------------------------------------------------------------");
+        for (double degrees = -5.0; degrees <= 150.0; degrees += 5.0) {
+            AngleD pivotAngle = new AngleD().setDegrees(degrees);
+            double pivotPosition = ArmGeometry.getPivotPositionFromAngle(pivotAngle);
+            // this is the computation of maximum arm extension position from Ethan's
+            // original code.
+            double distInches = 39.5 / pivotAngle.sin();
+            double extensionPosition = 200 - (distInches * 3.377 * 0.9);
+            extensionPosition = Utl.clip(extensionPosition, 0, 210);
+
+            Point2D.Double location = ArmGeometry.getArmLocationFromPositions(
+                    pivotPosition, extensionPosition);
+            System.out.println(String.format(
+                    "angle(deg): %7.2f; positions: (%7.2f,%7.2f); back to location: (%7.2f,%7.2f)",
+                    degrees, pivotPosition, extensionPosition, location.x, location.y));
+        }
+    }
+
 
 }
