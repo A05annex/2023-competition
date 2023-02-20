975a4fa3d8c56c6a5a45f31aed91c8c247081304
STHobbes <royster.hall@gmail.com>
2023-02-19 21:30:47 -0700

    Removed superfluous stuff in OneMeterDriveCommand.

--- src/main/java/frc/robot/commands/OneMeterDriveCommand.java
+++ src/main/java/frc/robot/commands/OneMeterDriveCommand.java
@@ -2,29 +2,13 @@ package frc.robot.commands;
 
 import org.a05annex.frc.commands.AbsoluteTranslateCommand;
 
+/**
+ * Extend {@link AbsoluteTranslateCommand} to explicitly construct a 1m left
+ * strafe translation with a no argument constructor.
+ */
 public class OneMeterDriveCommand extends AbsoluteTranslateCommand {
 
     public OneMeterDriveCommand() {
         super(0.0, 1.0);
-    }
-
-    @Override
-    public void initialize() {
-        super.initialize();
-    }
-
-    @Override
-    public void execute() {
-        super.execute();
-    }
-
-    @Override
-    public boolean isFinished() {
-        return super.isFinished();
-    }
-
-    @Override
-    public void end(boolean interrupted) {
-        super.end(interrupted);
     }
 }
