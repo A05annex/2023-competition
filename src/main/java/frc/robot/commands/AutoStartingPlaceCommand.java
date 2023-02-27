3c1b7a260080db2a76c551aee03e32103dd7f16b
Ethan Ready <ethanready@icloud.com>
2023-02-26 17:27:19 -0800

    Added startingPlaceCommand to balance auto

--- src/main/java/frc/robot/commands/AutoStartingPlaceCommand.java
+++ src/main/java/frc/robot/commands/AutoStartingPlaceCommand.java
@@ -9,7 +9,7 @@ public class AutoStartingPlaceCommand extends CommandBase {
     private final ArmSubsystem armSubsystem = ArmSubsystem.getInstance();
     private final ClawSubsystem clawSubsystem = ClawSubsystem.getInstance();
 
-    private final double PIVOT = 18.0;
+    private final double PIVOT = -17.0;
     private final double EXTENSION = 56.0;
     private final double DEADBAND = 0.5;
     private boolean isFinished = false;
@@ -46,7 +46,7 @@ public class AutoStartingPlaceCommand extends CommandBase {
         } else if (ticks == 5) {
             clawSubsystem.off();
             ticks++;
-        } else if(ticks >= 75) {
+        } else if(ticks >= 25) {
             isFinished = true;
         } else if (ticks > 0) {
             ticks++;
