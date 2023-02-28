3f3f19fd763bce9d4e14b46a132f3e88d6074767
Ethan Ready <ethanready@icloud.com>
2023-02-28 10:30:43 -0800

    Updated AutoBalanceCommand to have rotation control to lock up or down field

--- src/main/java/frc/robot/commands/AutoBalanceCommand.java
+++ src/main/java/frc/robot/commands/AutoBalanceCommand.java
@@ -1,7 +1,7 @@
 package frc.robot.commands;
 
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import org.a05annex.frc.A05Constants;
 import org.a05annex.frc.NavX;
 import org.a05annex.frc.subsystems.DriveSubsystem;
 import org.a05annex.util.AngleConstantD;
@@ -11,24 +11,30 @@ import org.a05annex.util.AngleD;
  * AutoBalanceCommand moves robot forward or backward based on robot tilt
  */
 public class AutoBalanceCommand extends CommandBase {
-    private final DriveSubsystem m_driveSubsystem = DriveSubsystem.getInstance();
-    private final NavX m_navX = NavX.getInstance();
+    private final DriveSubsystem driveSubsystem = DriveSubsystem.getInstance();
+    private final NavX navX = NavX.getInstance();
     // you need to use roll as pitch and roll return the opposite values of what you expect
-    private AngleConstantD m_pitch = m_navX.getNavInfo().roll;
+    private AngleConstantD m_pitch = navX.getNavInfo().roll;
 
     // how fast should the robot drive (0.0 - 1.0) when trying to balance
-    private final double m_speed = 0.15;
-    private final double m_angle = 10.0;
+    private final double SPEED = 0.15;
+    // At what degree angle should the robot stop moving. (platform max tilt is 15°)
+    private final double ANGLE = 10.0;
+
+    private double upField = navX.getHeadingInfo().getClosestDownOrUpField() == navX.getHeadingInfo().getClosestUpField() ? 1.0 : -1.0;
+
+    private double speed = 0.0, rotation = 0.0;
 
     // track during how many cycles (20ms) the robot was balanced
     private int ticksBalanced = 0;
+
     private boolean isFinished = false;
 
 
     public AutoBalanceCommand() {
         // each subsystem used by the command must be passed into the
         // addRequirements() method (which takes a vararg of Subsystem)
-        addRequirements(m_driveSubsystem);
+        addRequirements(this.driveSubsystem);
     }
 
     @Override
@@ -36,36 +42,31 @@ public class AutoBalanceCommand extends CommandBase {
         isFinished = false;
         ticksBalanced = 0;
         m_pitch = NavX.getInstance().getNavInfo().roll;
-
-        // set expected heading to either 0 or 180, whichever is closer to current heading
-        if (m_navX.getHeading().isGreaterThan(AngleConstantD.DEG_NEG_90) && m_navX.getHeading().isLessThanOrEqual(AngleConstantD.DEG_90)) {
-            m_navX.setExpectedHeading(new AngleD().setDegrees(0.0));
-        }
-        else {
-            m_navX.setExpectedHeading(new AngleD().setDegrees(180.0));
-        }
     }
 
     @Override
     public void execute() {
+        upField = navX.getHeadingInfo().getClosestDownOrUpField() == navX.getHeadingInfo().getClosestUpField() ? 1.0 : -1.0;
+
         m_pitch = NavX.getInstance().getNavInfo().roll; // set pitch again because getNavInfo does not auto update
-        if (m_pitch.getDegrees() > m_angle) {
+        if (m_pitch.getDegrees() > -ANGLE * upField) {
             // drive backward and reset ticks balanced when tipped forward
-            m_driveSubsystem.swerveDrive(AngleConstantD.ZERO, m_speed, 0.0);
+            speed = -SPEED;
             ticksBalanced = 0;
-        } else if (m_pitch.getDegrees() < -m_angle) {
+        } else if (m_pitch.getDegrees() < ANGLE * upField) {
             // drive forward and reset ticks balanced when tipped backward
-            m_driveSubsystem.swerveDrive(AngleConstantD.ZERO, -m_speed, 0.0);
+            speed = SPEED;
             ticksBalanced = 0;
         } else {
             // Stop robot and increment ticks balanced while not at full tilt
-            m_driveSubsystem.swerveDrive(AngleConstantD.ZERO, 0.0, 0.0);
+            speed = 0.0;
             ticksBalanced++;
         }
 
-        SmartDashboard.putNumber("pitch", m_pitch.getDegrees());
-        SmartDashboard.putNumber("ticks balanced", ticksBalanced);
-        SmartDashboard.putBoolean("is finished", isFinished);
+        navX.setExpectedHeading(navX.getHeadingInfo().getClosestDownOrUpField());
+        rotation = new AngleD(navX.getHeadingInfo().expectedHeading).subtract(new AngleD(navX.getHeadingInfo().heading))
+                .getRadians() * A05Constants.getDriveOrientationkp();
+        driveSubsystem.swerveDrive(AngleConstantD.ZERO, speed, rotation);
     }
 
     @Override
