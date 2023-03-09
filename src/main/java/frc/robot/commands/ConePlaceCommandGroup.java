422fdd8abf6cb9299edb1ec6aa3b79d2cc043e5b
Ethan Ready <ethanready@icloud.com>
2023-03-08 19:48:20 -0800

    Robot calibration constants and accel decel added to cone place translate commands

--- src/main/java/frc/robot/commands/ConePlaceCommandGroup.java
+++ src/main/java/frc/robot/commands/ConePlaceCommandGroup.java
@@ -5,6 +5,7 @@ import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.ConditionalCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import org.a05annex.frc.A05Constants;
+import org.a05annex.frc.commands.AbsoluteSmartTranslateCommand;
 import org.a05annex.frc.commands.AbsoluteTranslateCommand;
 
 public class ConePlaceCommandGroup extends SequentialCommandGroup {
@@ -12,13 +13,13 @@ public class ConePlaceCommandGroup extends SequentialCommandGroup {
 
     public ConePlaceCommandGroup(XboxController altXbox, A05Constants.DriverSettings driver) {
         super(new ConePositionCommand(altXbox, driver),
-                new ConditionalCommand(new AbsoluteTranslateCommand(0.0, -0.50, 0.5).withTimeout(1.5), new AbsoluteTranslateCommand(0.0, 0.57).withTimeout(1.5), ConePlaceCommandGroup::direction),
+                new ConditionalCommand(new AbsoluteSmartTranslateCommand(0.0, -0.50, 1.0, 7000.0).withTimeout(1.5), new AbsoluteSmartTranslateCommand(0.0, 0.57, 1.0, 7000.0).withTimeout(1.5), ConePlaceCommandGroup::getDirection),
                 new AbsoluteTranslateCommand(0.65, 0.0, 0.5).withTimeout(1.5),
                 new ConeArmMoveCommand(altXbox));
         ConePlaceCommandGroup.altXbox = altXbox;
     }
 
-    private static boolean direction() {
+    private static boolean getDirection() {
         return altXbox.getPOV() == 45 || altXbox.getPOV() == 90 || altXbox.getPOV() == 135;
     }
 }
